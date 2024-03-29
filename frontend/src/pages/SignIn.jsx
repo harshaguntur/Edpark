95a4b3859fd6c72ef9286bf99feecba6bb21dfc7
harsha <gunturharsha13@gmail.com>
2024-03-29 12:04:44 +0530

    axios for signin

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+
 const SignIn = () => {
+
+    const [formData,setFormData] = useState({
+      userPar : "",
+      userPass : "",
+    })
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+
+    const handelSubmit = ()=>{
+
+      axios.post("/api/SignIn",formData);
+
+
+    }
+  
+
   return (
 
     <h1>hi</h1>
