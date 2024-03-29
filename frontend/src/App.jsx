23a98b59d4b6bf702f8c977fd4c1c113e1861ebf
harsha <gunturharsha13@gmail.com>
2024-03-29 11:56:42 +0530

    init home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn/>} />
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
