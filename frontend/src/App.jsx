0a86dd185a3f2c7d1661bea664f50485b5aaec79
harsha <gunturharsha13@gmail.com>
2024-03-29 11:50:36 +0530

    init signin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,8 @@
+const SignIn = () => {
+  return (
+
+    <h1>hi</h1>
+
+  )
+}
+export default SignIn
